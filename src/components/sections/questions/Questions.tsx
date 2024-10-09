a10b03fde8dcb9b39203f9c4fd470b0234fe134d
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-10-09 08:41:05 +0300

    feat: add input ui element

--- src/components/sections/questions/Questions.tsx
+++ src/components/sections/questions/Questions.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import styles from './Questions.module.scss';
 import Image from 'next/image';
+import { Input } from '@/components/ui';
+import { EInputType } from '@/components/ui/input/Input';
+
+interface IFormData {
+  phone: string | null;
+  comment: string | null;
+  isAgree: boolean;
+}
 
 const Questions: React.FC = () => {
+  const [formData, setFormData] = useState<IFormData>({
+    phone: null,
+    comment: null,
+    isAgree: false,
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value || null,
+    }));
+  };
+
   return (
     <section>
       <div className="container">
@@ -13,8 +38,28 @@ const Questions: React.FC = () => {
           <div className={styles.form}>
             <h2 className={styles.title}>Остались вопросы?</h2>
             <p className={styles.description}>
-              Заполните форму ниже, и наш специалист свяжется с вами в ближайшее время.
+              Заполните форму ниже, и наш специалист свяжется с вами в ближайшее время.
             </p>
+            <Input
+              label="Телефон"
+              name="phone"
+              placeholder="+375 (99) 999-99-99"
+              required={true}
+              type={EInputType.PHONE}
+              value={formData.phone || ''}
+              onChange={handleInputChange}
+              isFlex={true}
+            />
+            <Input
+              label="Комментарий"
+              name="comment"
+              placeholder="Ваш комментарий"
+              required={false}
+              type={EInputType.TEXTAREA}
+              value={formData.comment || ''}
+              onChange={handleInputChange}
+              isFlex={true}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/ui/input/Input.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styles from './Input.module.scss';
+
+export enum EInputType {
+  TEXT = 'text',
+  EMAIL = 'email',
+  PASSWORD = 'password',
+  PHONE = 'tel',
+  RADIO = 'radio',
+  CHECKBOX = 'checkbox',
+  DATE = 'date',
+  FILE = 'file',
+  NUMBER = 'number',
+  TEXTAREA = 'textarea',
+}
+
+interface IInputProps {
+  label: string;
+  name?: string;
+  value: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void; // Обновлено для поддержки textarea
+  placeholder?: string;
+  required?: boolean;
+  type?: EInputType;
+  isFlex?: boolean;
+}
+
+const Input: React.FC<IInputProps> = ({
+  label,
+  name,
+  value,
+  onChange,
+  placeholder,
+  required = true,
+  type = EInputType.TEXT,
+  isFlex = false,
+}) => {
+  return (
+    <div className={`${styles.block} ${isFlex && styles.blockFlex}`}>
+      <p className={styles.label}>
+        {label}
+        <span>{required ? '*' : ''}</span>
+      </p>
+      {type === EInputType.TEXTAREA ? (
+        <textarea
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          className={styles.textarea}
+        />
+      ) : (
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          className={styles.input}
+          pattern="[+]{1}[0-9]{11,14}"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Input;
